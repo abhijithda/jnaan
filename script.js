85237d59cc2e407612ef8b53440b1109fa727b9f
Abhijith D.A <abhijith.da@gmail.com>
2015-02-19 17:21:37 -0800

    Support for expand and collapse rows added.
    Support for expand and collapse rows added.

--- script.js
+++ script.js
@@ -17,29 +17,86 @@ app.service('shoppingList', function($http, $q) {
   };
 });
 
+
 app.controller('tableExpandCollapseController',
   function($scope, shoppingList) {
+
+    $scope.displayItemDetails = {};
+
+
+    $scope.toggleDisplay = function(iType) {
+      console.log("Toggling display of " + iType + " from " + $scope.displayItemDetails[iType]);
+      $scope.displayItemDetails[iType] = $scope.displayItemDetails[iType] ? 0 : 1;
+      console.log(" to " + $scope.displayItemDetails[iType]);
+
+      console.log(" All display details value:");
+      console.log($scope.displayItemDetails);
+
+      $scope.selItems = $scope.updateTable();
+      console.log("In toggleDisplay()::selItems: ");
+      console.log($scope.selItems);
+    };
+
+
+    $scope.updateTable = function() {
+      var selItems = [];
+      for (var i in $scope.allItems) {
+        var iType = $scope.allItems[i]["Type"];
+
+        if (angular.isUndefined($scope.displayItemDetails[iType])) {
+          $scope.displayItemDetails[iType] = 0;
+        }
+
+        if (1 == $scope.displayItemDetails[iType]) {
+          $scope.allItems[i]["Summary"] = '-';
+          console.log("Expanding(" + $scope.allItems[i]["Summary"] + ") details of " + iType);
+        } else {
+          $scope.allItems[i]["Summary"] = '+';
+          console.log("Collapsing(" + $scope.allItems[i]["Summary"] + ") details of " + iType);
+        }
+        selItems.push($scope.allItems[i]);
+
+        console.log("Display details of " + iType + ": " + $scope.displayItemDetails[iType]);
+        if ($scope.displayItemDetails[iType]) {
+          console.log("Viewing details of " + iType);
+          for (var j in $scope.allItems[i]["Details"]) {
+            $scope.allItems[i]["Details"][j]["Summary"] = "";
+            selItems.push($scope.allItems[i]["Details"][j]);
+          }
+        }
+
+      }
+      console.log("In updateTable()::selItems: ");
+      console.log(selItems);
+      return selItems;
+    };
+
 
     shoppingList.getShoppingList().then(function(result) {
       console.log("Result: ");
       console.log(result);
       $scope.allItems = result.data.Items;
-      $scope.selItems = result.data.Items;
+      $scope.selItems = $scope.updateTable();
       console.log("In then::selItems: ");
       console.log($scope.selItems);
-
     });
 
 
     $scope.gridOptions = {
-     data: 'selItems',
+      data: 'selItems',
       columnDefs: [{
         field: 'Summary',
         displayName: '',
-        cellTemplate: '<div class="ngCellText"> <div ng-if="row.getProperty(col.field)"> <button ng-click="toggleDisplay(row.entity.Type)"> {{row.getProperty(col.field)}} </button> </div> </div>'
+        cellTemplate: "<div ng-if='row.getProperty(col.field)'>" +
+          "<button ng-click='toggleDisplay(row.entity.Type)'> {{row.getProperty(col.field)}} </button>" +
+          "</div>",
+        width: 30
       }, {
         field: 'Name',
         displayName: 'Name',
+      }, {
+        field: 'Type',
+        displayName: 'Type',
       }, {
         field: 'Cost',
         displayName: 'Cost',
@@ -50,5 +107,7 @@ app.controller('tableExpandCollapseController',
       enableCellSelection: false,
       enableColumnResize: true
     };
- // });
+
   });
+  
+  
